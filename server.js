089dfacd6a683019ee9ac9845771e62ed065ba71
mentir-lab <vip.dron.2004@mail.ru>
2026-09-18 19:48:42 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,48 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    let filePath = '.' + req.url;
+    if (filePath === './') {
+        filePath = './Автозаполнение.html'; // или название твоего HTML файла
+    }
+
+    const extname = path.extname(filePath);
+    let contentType = 'text/html';
+    
+    const extToContentType = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.json': 'application/json',
+        '.png': 'image/png',
+        '.jpg': 'image/jpg',
+        '.gif': 'image/gif',
+    };
+    
+    if (extToContentType[extname]) {
+        contentType = extToContentType[extname];
+    }
+
+    fs.readFile(filePath, (error, content) => {
+        if (error) {
+            if(error.code === 'ENOENT') {
+                res.writeHead(404);
+                res.end('File not found');
+            } else {
+                res.writeHead(500);
+                res.end('Server error');
+            }
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf-8');
+        }
+    });
+});
+
+const port = 3000;
+server.listen(port, () => {
+    console.log(`Сервер запущен на http://localhost:${port}`);
+    console.log(`Открой в браузере: http://localhost:${port}`);
+});
